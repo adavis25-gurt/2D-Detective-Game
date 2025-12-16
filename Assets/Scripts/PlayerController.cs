f08526665de1410c45804c76838557368dbd957d
adavis25-gurt <adavis25@elam.co.uk>
2025-12-16 15:38:58 +0000

    added collisions and like fixed sprites
    MNAKE SURE THE PPU PER SPRITE IS ALWAYS 32

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+
+    public LayerMask stopsMovement;
+
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        {
+            if (Mathf.Abs(horizontal) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(horizontal, 0f, 0f), .05f, stopsMovement))
+                {
+                    vertical = 0f;
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+            }
+
+            if (Mathf.Abs(vertical) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, vertical, 0f), .05f, stopsMovement))
+                {
+                    horizontal = 0f;
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+            }
+        }
+    }
+}
